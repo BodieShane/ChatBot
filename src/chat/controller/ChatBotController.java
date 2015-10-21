a838ed458de96ab42571a20b9eb733717f63e334
Bodie Shane <bodieshane68@gmail.com>
2015-10-21 08:29:04 -0600

    Added a runner

---
+++ src/chat/controller/ChatBotController.java
@@ -0,0 +1,14 @@
+package chat.controller;
+/**
+ * Application controller for the Chatbot project.
+ * @author bsha6756
+ *@version 1.0 10/21/15
+ */
+public class ChatBotController 
+{
+	
+	public void start()
+	{
+		
+	}
+}
---
+++ src/chat/controller/ChatBotRunner.java
@@ -0,0 +1,17 @@
+package chat.controller;
+/**
+ * Starts the ChatBot program.
+ * @author bsha6756
+ * @version 1.0 10/21/15
+ */
+public class ChatBotRunner 
+{	
+	
+	public static void main(String[] args) 
+
+	{
+		ChatBotController myController = new ChatBotController();
+		myController.start();
+	}
+
+}
