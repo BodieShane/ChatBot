900e97c8d84169d184e5848bc92a64955bf99f7f
Bodie Shane <bodieshane68@gmail.com>
2015-10-21 09:56:10 -0600

    Tried to add user input to the program

--- src/chat/controller/ChatBotController.java
+++ src/chat/controller/ChatBotController.java
@@ -1,6 +1,7 @@
 package chat.controller;
 import javax.swing.JOptionPane;
 import chat.view.ChatView;
+import java.util.Scanner;
 /**
  * Application controller for the Chatbot project.
  * @author bsha6756
@@ -9,11 +10,23 @@ import chat.view.ChatView;
 public class ChatBotController 
 {
 	private ChatView myDisplay;
+	private Scanner ChatBotScanner;
+	private String Text;
+	
+	public ChatBotController()
+	{
+		
+	}
 	
 	
 	public void start()
 	{
 		JOptionPane.showMessageDialog(null,"HAHAHAHA I WILL LIVE ON IN EVREY PROJRAM");
 		JOptionPane.showMessageDialog(null,"YOU WILL NEVER STOP ME!!!!!");
+		String Text = ChatBotScanner .next();
+		JOptionPane.showMessageDialog(null,"You think saying " + Text + " will stop me?");
+		
+		
+		
 	}
 }
