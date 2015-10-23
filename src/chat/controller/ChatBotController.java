92782eee11db9d6659fe7b7b8ccf6a15a3f3590b
Bodie Shane <bodieshane68@gmail.com>
2015-10-23 08:20:13 -0600

    Length Checker was added

--- src/chat/controller/ChatBotController.java
+++ src/chat/controller/ChatBotController.java
@@ -12,21 +12,38 @@ public class ChatBotController
 	private ChatView myDisplay;
 	private Scanner ChatBotScanner;
 	private String Text;
+	private String  Name;
+	private String  MyName;
+	private String TextName;
+	
+	
 	
 	public ChatBotController()
 	{
+		
+		
+		
+		ChatBotScanner = new Scanner(System.in);
 		
 	}
 	
 	
 	public void start()
 	{
-		JOptionPane.showMessageDialog(null,"HAHAHAHA I WILL LIVE ON IN EVREY PROJRAM");
-		JOptionPane.showMessageDialog(null,"YOU WILL NEVER STOP ME!!!!!");
+		System.out.print("HAHAHAHA I WILL LIVE ON IN EVREY PROJRAM");
+		System.out.print("YOU WILL NEVER STOP ME!!!!! WHAT CAN YOU EVEN DO");
 		String Text = ChatBotScanner .next();
-		JOptionPane.showMessageDialog(null,"You think saying " + Text + " will stop me?");
 		
+		System.out.print("You think saying " + Text + " will NEVER be enough to stop me?");
+		System.out.print(" Tell me kid what is your name?");
+		String Name = ChatBotScanner .next();
 		
+		System.out.print(" well hello " + Name + " what is my name?");
+		String MyName = ChatBotScanner .next();
+		
+		System.out.print("So my name is " + MyName + ". You know what I like your name better we can just swich");
+		System.out.print("So my name is " + Name + " and you are " + MyName + ". What do you think about that?");
+		String TextName = ChatBotScanner .next();
 		
 	}
 }
