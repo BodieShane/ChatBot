9c4b22c34572957890c05bfe077b120c30750e4d
Bodie Shane <bodieshane68@gmail.com>
2015-10-28 09:55:30 -0600

    we added a meam checker.

--- src/chat/controller/ChatBotController.java
+++ src/chat/controller/ChatBotController.java
@@ -1,7 +1,10 @@
 package chat.controller;
 import javax.swing.JOptionPane;
+
 import chat.model.Chatbot;
 import chat.view.ChatView;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -14,11 +17,15 @@ public class ChatBotController
 	
 	private ChatView myDisplay;
 	private Scanner ChatBotScanner;
-	private String Text;
-	private String  Name;
-	private String  MyName;
-	private String TextName;
+	//private String Text;
+	//private String  Name;
+	//private String  MyName;
+	//private String TextName;
 	private Chatbot myBot;
+	private ArrayList<String> memesList;
+	private ArrayList<String> politicalTopicList;
+	private String userName;
+	private String content;
 	
 	
 	public ChatBotController()
@@ -26,8 +33,7 @@ public class ChatBotController
 		myDisplay = new ChatView();
 		String userName= myDisplay.grabText("What is your name?");
 		myBot = new Chatbot(userName);
-		//String userName = MyName;
-		//myBot = new Chatbot();
+		
 		
 		ChatBotScanner = new Scanner(System.in);
 		
@@ -35,26 +41,47 @@ public class ChatBotController
 	
 	
 	public void start()
+
 	{
 		myDisplay.displaymessage("Hello " + myBot.getUserName());
 		
-		JOptionPane.showMessageDialog(null, "HAHAHAHA I WILL LIVE ON IN EVREY PROJRAM");
-		Text = JOptionPane.showInputDialog(null, "YOU WILL NEVER STOP ME!!!!! WHAT CAN YOU EVEN DO");
+		chat();
+		
+		//JOptionPane.showMessageDialog(null, "HAHAHAHA I WILL LIVE ON IN EVREY PROJRAM");
+		//Text = JOptionPane.showInputDialog(null, "YOU WILL NEVER STOP ME!!!!! WHAT CAN YOU EVEN DO");
 		
 		
-		JOptionPane.showMessageDialog(null, "You think saying " + Text + " will scare me? You will NEVER be enough to stop me?");
-		Name = JOptionPane.showInputDialog(null," Tell me kid what is your name?");
+		//JOptionPane.showMessageDialog(null, "You think saying " + Text + " will scare me? You will NEVER be enough to stop me?");
+		//Name = JOptionPane.showInputDialog(null," Tell me kid what is your name?");
 		 
-		MyName = JOptionPane.showInputDialog(null, " well hello " + Name + " what is my name?");
+		//MyName = JOptionPane.showInputDialog(null, " well hello " + Name + " what is my name?");
 		
 		
 		
-		JOptionPane.showMessageDialog(null,"So my name is " + MyName + ". You know what I like your name better we can just swich");
+		//JOptionPane.showMessageDialog(null,"So my name is " + MyName + ". You know what I like your name better we can just swich");
 		
-		TextName = JOptionPane.showInputDialog(null, "So my name is " + Name + " and you are " + MyName + ". What do you think about that?");
+		//TextName = JOptionPane.showInputDialog(null, "So my name is " + Name + " and you are " + MyName + ". What do you think about that?");
 		
-		Text = JOptionPane.showInputDialog(null, "Yes I see " + MyName +" You know what that's a bad name what do you want to change it to?" );
-		JOptionPane.showMessageDialog(null," ok your new name is 'But face' ");
+		//Text = JOptionPane.showInputDialog(null, "Yes I see " + MyName +" You know what that's a bad name what do you want to change it to?" );
+		//JOptionPane.showMessageDialog(null," ok your new name is 'But face' ");
 		
+		
+		
+	
+	}
+	
+	private void chat() 
+	
+	{
+		String conversation = myDisplay.grabText("What would you like to talk about today?");
+		while(myBot.lengthChecker(conversation))
+		{
+			if(myBot.contentChecker(conversation))
+			{
+				myDisplay.displaymessage("wow, I had no idea you are interested in" + myBot.getContent());
+			}
+			
+			conversation = myDisplay.grabText(conversation);
+		}
 	}
 }
--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,11 +1,17 @@
 package chat.model;
+
 import chat.controller.ChatBotController;
+
 import java.util.ArrayList;
 
+import javax.print.DocFlavor.STRING;
+
 /**
- * Base version of the 2015 Chatbot class. Only stub methods are provided. Students will complete methods as part
- * of the project. Added pop ups to the project
- * @author Bodie Shane 
+ * Base version of the 2015 Chatbot class. Only stub methods are provided.
+ * Students will complete methods as part of the project. Added pop ups to the
+ * project
+ * 
+ * @author Bodie Shane
  * @version 1.3 10/24/15
  */
 public class Chatbot
@@ -14,126 +20,186 @@ public class Chatbot
 	private ArrayList<String> politicalTopicList;
 	private String userName;
 	private String content;
-	
+
 	/**
 	 * Creates an instance of the Chatbot with the supplied username.
-	 * @param userName The username for the chatbot.
+	 * 
+	 * @param userName
+	 *            The username for the chatbot.
 	 */
 	public Chatbot(String userName)
 	{
-		this.userName= userName;
+		this.userName = userName;
+		this.content = "Game's";
+		this.memesList = new ArrayList<String>();
+		this.politicalTopicList = new ArrayList<String>();
+		buildMemesList();
+		buildPoliticalTopicsList();
 	}
-	
+
 	private void buildMemesList()
 	{
-		
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("troll ");
+		this.memesList.add("what if i told you ");
+		this.memesList.add("rare pepe ");
+		this.memesList.add("aliens ");
+		this.memesList.add("doge ");
+		this.memesList.add("bad luck brian ");
+		this.memesList.add("unhelpful highschool teacher");
+		this.memesList.add("spoderman");
+		this.memesList.add("cute animals ");
+		this.memesList.add("JOHN CENA ");
+
 	}
-	
+
 	private void buildPoliticalTopicsList()
 	{
-		
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+		this.memesList.add("me gusta y no ");
+
 	}
-	
+
 	/**
-	 * Checks the length of the supplied string. Returns false if the supplied String is empty or null,
-	 * otherwise returns true. 
+	 * Checks the length of the supplied string. Returns false if the supplied
+	 * String is empty or null, otherwise returns true.
+	 * 
 	 * @param currentInput
 	 * @return A true or false based on the length of the supplied String.
 	 */
 	public boolean lengthChecker(String currentInput)
 	{
 		boolean hasLength = false;
-		
-		if(currentInput != null)
+
+		if (currentInput != null)
 		{
-			if(currentInput.length() >= 1 )
+			if (currentInput.length() >= 1)
 			{
 				hasLength = true;
 			}
 		}
-		
-		if(currentInput != null && currentInput.length() > 0)
+
+		if (currentInput != null && currentInput.length() > 0)
 		{
 			hasLength = true;
 		}
-			
+
 		return hasLength;
 	}
-	
+
 	/**
-	 * Checks if the supplied String matches the content area for this Chatbot instance.
-	 * @param currentInput The supplied String to be checked.
+	 * Checks if the supplied String matches the content area for this Chatbot
+	 * instance.
+	 * 
+	 * @param currentInput
+	 *            The supplied String to be checked.
 	 * @return Whether it matches the content area.
 	 */
 	public boolean contentChecker(String currentInput)
 	{
-		return false;
+		boolean hascotent = false;
+
+		if (currentInput.toLowerCase().contains(content.toLowerCase()))
+		{
+			hascotent = true;
+		}
+
+		return hascotent;
 	}
-	
+
 	/**
-	 * Checks if supplied String matches ANY of the topics in the politicalTopicsList. Returns
-	 * true if it does find a match and false if it does not match.
-	 * @param currentInput The supplied String to be checked.
+	 * Checks if supplied String matches ANY of the topics in the
+	 * politicalTopicsList. Returns true if it does find a match and false if it
+	 * does not match.
+	 * 
+	 * @param currentInput
+	 *            The supplied String to be checked.
 	 * @return Whether the String is contained in the ArrayList.
 	 */
 	public boolean politicalTopicChecker(String currentInput)
 	{
 		return false;
 	}
-	
-	
+
 	/**
-	 * Checks to see that the supplied String value is in the current memesList variable.
-	 * @param currentInput The supplied String to be checked.
+	 * Checks to see that the supplied String value is in the current memesList
+	 * variable.
+	 * 
+	 * @param currentInput
+	 *            The supplied String to be checked.
 	 * @return Whether the supplied String is a recognized meme.
 	 */
 	public boolean memeChecker(String currentInput)
 	{
-		return false;
-	}
+		boolean hasMeme = false;
+		for (STRING currentMeme:memesList )
+		{
+			if(currentMeme.equalsignore()
+					{
+					 
+					}
+		}
 	
+		
+		return hasMeme;
+	}
+
 	/**
 	 * Returns the username of this Chatbot instance.
+	 * 
 	 * @return The username of the Chatbot.
 	 */
 	public String getUserName()
 	{
 		return userName;
 	}
-	
+
 	/**
 	 * Returns the content area for this Chatbot instance.
+	 * 
 	 * @return The content area for this Chatbot instance.
 	 */
 	public String getContent()
 	{
-		return null;
+		return content;
 	}
-	
+
 	/**
 	 * Getter method for the memesList object.
+	 * 
 	 * @return The reference to the meme list.
 	 */
 	public ArrayList<String> getMemesList()
 	{
-		return null;
+		return memesList;
 	}
-	
+
 	/**
 	 * Getter method for the politicalTopicList object.
+	 * 
 	 * @return The reference to the political topic list.
 	 */
 	public ArrayList<String> getPoliticalTopicList()
 	{
 		return null;
 	}
-	
+
 	/**
 	 * Updates the content area for this Chatbot instance.
-	 * @param content The updated value for the content area.
+	 * 
+	 * @param content
+	 *            The updated value for the content area.
 	 */
 	public void setContent(String content)
 	{
-		
+
 	}
 }
