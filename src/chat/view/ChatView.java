f88be60aa5fc2a9b8d4c9cf2d17e215ecb3d6ef5
Bodie Shane <bodieshane68@gmail.com>
2015-11-05 08:50:14 -0700

    Added a new image to the pop up window.

--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -1,22 +1,43 @@
 package chat.view;
 
+/**
+ * Base version of the 2015 ChatbotView class. 
+ * 
+ * @author Bodie Shane
+ * @version 1.3 11/5/15 Addedd popup windows Icons!
+ */
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
+
 
 public class ChatView 
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
 
+	public ChatView()
+	{
+		windowMessage = "Hello form your no so friendly Chat Bot : )";
+		chatIcon = new ImageIcon(getClass().getResource("images/MyRobotFace.png"));
+			
+	}
 	
 	public  void displaymessage(String message)
 	{
-		JOptionPane.showMessageDialog(null, message);
+		JOptionPane.showMessageDialog(null, message,windowMessage, JOptionPane.INFORMATION_MESSAGE,chatIcon );
 		
 	}
 	
 	public String grabText (String message)
 	{
 		String output = "";
-		output = JOptionPane.showInputDialog(null, message);
+		
+		output = JOptionPane.showInputDialog(null, message, windowMessage, JOptionPane.PLAIN_MESSAGE,chatIcon,null,"'But Face'").toString();
+		
 		return output;
+
 				
 	}
+	
 }
