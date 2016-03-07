ab65472c044dfd8ae5129c5c216d547865cda0b1
Bodie Shane <bodieshane68@gmail.com>
2016-03-07 09:15:00 -0700

    Added the send Tweet Methed

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -16,11 +16,25 @@ public class CTECTwitter
 	
 	private ArrayList <Status> statusList;
 	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
 
 
 	public CTECTwitter()
 	{
 		statusList = new ArrayList<Status>();
 		wordList = new ArrayList<String>();
+	}
+	
+	public void sendTweet(String tweet)
+	{
+		try
+		{
+			chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+		}
+		catch (TwitterException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
